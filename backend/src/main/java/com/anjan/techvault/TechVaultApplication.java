c8720f0d5a9d4149b4b74f6a2d31a1a255978712
anjanx44 <anjanx44@gmail.com>
2025-01-19 22:17:37 +0600

    project init

---
+++ backend/src/main/java/com/anjan/techvault/TechVaultApplication.java
@@ -0,0 +1,13 @@
+package com.anjan.techvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechVaultApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/anjan/techvault/TechVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjan.techvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
