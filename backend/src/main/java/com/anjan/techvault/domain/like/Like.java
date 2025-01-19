5800b477eb3e43bf7b25166ceded7b167803a6aa
anjanx44 <anjanx44@gmail.com>
2025-01-19 23:13:54 +0600

    fix postgres table name issue

--- backend/src/main/java/com/anjan/techvault/domain/like/Like.java
+++ backend/src/main/java/com/anjan/techvault/domain/like/Like.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "likes")
 public class Like {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
